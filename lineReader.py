b2e01e3660a6df7e61ced1679ef99fbdb0539bd0
caozy <cao5zy@163.com>
2017-11-30 09:38:35 +0800

    add debug code

--- lineReader.py
+++ lineReader.py
@@ -6,13 +6,18 @@ def lineReader(content):
 	return lambda lineReaderConfig: list(filter(lambda line: line != None, map(lambda line: processLine(line, lineReaderConfig), content.splitlines())))
 
 def processLine(line, lineReaderConfig):
+	def debugResult(result):
+		#print({'line':line, 'result':result, 'config': lineReaderConfig.useItWhenTrue, \
+		#	'rePattern': lineReaderConfig.rePattern})
+		return result
+
 	def handleResultWhenHasNoGroup(result):
 		return result.group()
 	def handleResultWhenHasGroup(result):
 		return result.groups()[lineReaderConfig.getGroupNumber()]
 
-	return (lambda result: handleResultWhenHasGroup(result)  \
+	return debugResult((lambda result: handleResultWhenHasGroup(result)  \
 	if result and lineReaderConfig.useItWhenTrue and lineReaderConfig.hasGroup \
 	else handleResultWhenHasNoGroup(result) if result and lineReaderConfig.useItWhenTrue \
 	else line if not (result or lineReaderConfig.useItWhenTrue) \
-	else None)(re.search(lineReaderConfig.rePattern, line))
+	else None)(re.search(lineReaderConfig.rePattern, line)))
